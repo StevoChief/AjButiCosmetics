e13e29ff7a587435880e0915fd02108b63d37ea8
StevoChief <stevochief10@gmail.com>
2020-05-29 18:44:04 -0400

    Shopping Cart Code

--- store.js
+++ store.js
@@ -1,19 +1,9 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+simpleCart({
+	checkout: { 
+		type: "PayPal" , 
+		email: "stevochief10@gmail.com" 
+  },
 
-// Get the header
-var header = document.getElementById("myHeader");
-var menu = document.getElementById("btn");
-var exit = document.getElementById("cancel");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+  cartStyle: "table"
+  
+});
